03725929c831ab14e183964bb823947844eea935
CelestrialDragon <gustav.tue@gmail.com>
2026-03-17 09:09:27 +0100

    Update train.py

--- train.py
+++ train.py
@@ -18,7 +18,11 @@ n = 6
 
 def matricies_init_ ():
     dictonary_vectors = np.array([[ random.uniform(-1, 1) for _ in range(d_model)] for _ in range(d_size)])
-    multihead_matrices = np.array([[[ random.uniform(- math.sqrt(6/(fan_in+fan_out)), math.sqrt(6/(fan_in+fan_out))) for _ in range(qkv)] for _ in range(d_model)] for _ in range(n)])
-    return dictonary_vectors, multihead_matrices
-dictonary_vectors, multihead_matrices = matricies_init_ ()
+    multihead_matrices_input = np.array([[[ random.uniform(- math.sqrt(6/(fan_in+fan_out)), math.sqrt(6/(fan_in+fan_out))) for _ in range(qkv)] for _ in range(d_model)] for _ in range(n)])
+    multihead_matrices_output = np.array([[[ random.uniform(- math.sqrt(6/(fan_in+fan_out)), math.sqrt(6/(fan_in+fan_out))) for _ in range(qkv)] for _ in range(d_model)] for _ in range(n)])
+    return dictonary_vectors, multihead_matrices_input, multihead_matrices_output
+dictonary_vectors, multihead_matrices_input, multihead_matrices_output = matricies_init_ ()
+
+
+
 print(multihead_matrices[0][0])
